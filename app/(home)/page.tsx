ac4f1a0e742cae3a11303f470d4e942788bc4452
MarceloALMoreira <marceloaomoreira@hotmail.com>
2024-01-30 14:51:38 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,7 @@ import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarberShopItem from "./_components/barbershop-item";
+import Footer from "../_components/footer";
 
 export default async function Home() {
   // chamar prisma e pegar barbearias
@@ -36,6 +37,17 @@ export default async function Home() {
       <div className="mt-6 px-5">
         <h2 className="mb-3 text-xs font-bold uppercase text-gray-400">
           Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbarshops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mb-[4.5rem] mt-6 px-5">
+        <h2 className="mb-3 text-xs font-bold uppercase text-gray-400">
+          Populares
         </h2>
         <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbarshops.map((barbershop) => (
