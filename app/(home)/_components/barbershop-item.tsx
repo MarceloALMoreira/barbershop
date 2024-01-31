34ee875e7e09fa3cf5f3345862be61b8c4eade7a
MarceloALMoreira <marceloaomoreira@hotmail.com>
2024-01-31 11:20:21 -0300

    feat: add BarbershopInf component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,13 +1,21 @@
+"use client";
+
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 interface BarberShopItemProps {
   barbershop: Barbershop;
 }
 const BarberShopItem = ({ barbershop }: BarberShopItemProps) => {
+  const router = useRouter();
+
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`);
+  };
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="px-1 py-0">
@@ -40,7 +48,11 @@ const BarberShopItem = ({ barbershop }: BarberShopItemProps) => {
           <p className="overflow-hidden text-ellipsis text-nowrap text-sm text-gray-400">
             {barbershop.address}
           </p>
-          <Button variant="secondary" className="mt-3 w-full">
+          <Button
+            variant="secondary"
+            className="mt-3 w-full"
+            onClick={handleBookingClick}
+          >
             Reservar
           </Button>
         </div>
