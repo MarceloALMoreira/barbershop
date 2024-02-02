9ab5deac26cb50f381ca3b855e682fc17650414a
MarceloALMoreira <marceloaomoreira@hotmail.com>
2024-02-02 10:03:36 -0300

    fix: add coment service item

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -117,10 +117,12 @@ const ServiceItem = ({
       return [];
     }
 
+    //filtro nos hoario agendados
     return generateDayTimeList(date).filter((time) => {
       const timeHour = Number(time.split(":")[0]);
       const timeMinutes = Number(time.split(":")[1]);
 
+      // verifica se tem agendando com o mesmo horario e mesmo minuto
       const booking = dayBookings.find((booking) => {
         const bookingHour = booking.date.getHours();
         const bookingMinutes = booking.date.getMinutes();
