ed7eccbc918afe58a355c02d91e8c86a303d513c
MarceloALMoreira <marceloaomoreira@hotmail.com>
2024-02-04 22:51:24 -0300

    feat: ensuresematic tags are used to footer and header

--- app/_components/ui/header.tsx
+++ app/_components/ui/header.tsx
@@ -11,24 +11,26 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <Card>
-      <CardContent className="flex flex-row items-center justify-between p-5  py-8">
-        <Link href="/">
-          <Image src={logo} alt="logo-barber-shop" width={120} height={18} />
-        </Link>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon" className="h-8 w-8">
-              <MenuIcon size={16} />
-            </Button>
-          </SheetTrigger>
+    <header>
+      <Card>
+        <CardContent className="flex flex-row items-center justify-between p-5  py-8">
+          <Link href="/">
+            <Image src={logo} alt="logo-barber-shop" width={120} height={18} />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" className="h-8 w-8">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
 
-          <SheetContent className="p-0">
-            <SideMenu />
-          </SheetContent>
-        </Sheet>
-      </CardContent>
-    </Card>
+            <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
   );
 };
 // 1:21
